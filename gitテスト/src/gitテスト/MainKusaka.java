76300bb6904fe1be0bc15262f606a93edf0e1514
outov <outov@MyComputer>
2021-07-08 11:51:02 +0900

    変更しました。

--- gitテスト/src/gitテスト/MainKusaka.java
+++ gitテスト/src/gitテスト/MainKusaka.java
@@ -4,7 +4,8 @@ public class MainKusaka {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		System.out.println("日下拓巳");
+		System.out.println("日下拓巳");]
+				System.out.println("変更しました。");
 
 	}
 }
