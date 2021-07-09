8f51bc899c5368991e791e2093f9d484fa185c93
sotamain <sotamain@LAPTOP-FT2PTUO1>
2021-07-09 10:36:03 +0900

    柚木に変更

--- gitテスト/src/gitテスト/MainKusaka.java
+++ gitテスト/src/gitテスト/MainKusaka.java
@@ -5,9 +5,7 @@ public class MainKusaka {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		System.out.println("日下拓巳");
-				System.out.println("４回目変更しました。");
-				System.out.println("次は、札幌です。");
-				System.out.println("次は、札幌です。");
+				System.out.println("柚木");
 
 	}
 }
