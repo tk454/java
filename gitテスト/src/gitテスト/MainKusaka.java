97513d531890af42777ba48ce35480ac77a2c8e1
sotamain <sotamain@LAPTOP-FT2PTUO1>
2021-07-09 10:10:42 +0900

    二回目を三回目に変更 次は札幌を追加

--- gitテスト/src/gitテスト/MainKusaka.java
+++ gitテスト/src/gitテスト/MainKusaka.java
@@ -5,7 +5,8 @@ public class MainKusaka {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		System.out.println("日下拓巳");
-				System.out.println("二回目変更しました。");
+				System.out.println("三回目変更しました。");
+				System.out.println("次は、札幌です。");
 
 	}
 }
